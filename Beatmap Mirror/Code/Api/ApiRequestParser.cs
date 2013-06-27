c53cd09431f0997b6c43ac26d90d9e345bef8b9b
Mizore <thehexide@gmail.com>
2013-06-27 09:44:16 +0300

    Adds Json.NET library, directly embedded.

---
+++ Beatmap Mirror/Code/Api/ApiRequestParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beatmap_Mirror.Code.Api
+{
+    public class ApiRequestParser
+    {
+        public static T Parse<T>(string data)
+        {
+
+
+            return default(T);
+        }
+    }
+}
---
+++ Beatmap Mirror/Code/Structures/ApiSearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beatmap_Mirror.Code.Structures
+{
+    public class ApiSearch
+    {
+        public string Status { get; set; }
+    }
+}
